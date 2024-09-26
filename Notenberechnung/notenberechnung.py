65211f14b85d3939cdc5f60c36c67772b277afc8
xgvx <grischa.vinogradov@gmail.com>
2024-09-26 09:41:01 +0200

    implemented Gut

--- Notenberechnung/notenberechnung.py
+++ Notenberechnung/notenberechnung.py
@@ -1,4 +1,8 @@
 def schriftliche_note_bestimmen(prozentwert: int) -> str:
     if(prozentwert > 100 or prozentwert < 0):
         raise ValueError
-    return "sehr gut"
+    if(prozentwert > 91):
+        return "sehr gut"
+    if(prozentwert > 80):
+        return "gut"
+    return "moment mal so nicht mein freund"
--- Notenberechnung/test_notenberechnung.py
+++ Notenberechnung/test_notenberechnung.py
@@ -23,6 +23,28 @@ def test_schriftliche_note_bestimmen__bottom_of_sehr_gut():
     # Assert
     assert ist_ergebnis == soll_ergebnis
 
+def test_schriftliche_note_bestimmen__top_of_gut():
+    # Arrange
+    prozent = 91
+    soll_ergebnis = "gut"
+
+    # Act
+    ist_ergebnis = schriftliche_note_bestimmen(prozent)
+
+    # Assert
+    assert ist_ergebnis == soll_ergebnis
+
+def test_schriftliche_note_bestimmen__bottom_of_gut():
+    # Arrange
+    prozent = 81
+    soll_ergebnis = "gut"
+
+    # Act
+    ist_ergebnis = schriftliche_note_bestimmen(prozent)
+
+    # Assert
+    assert ist_ergebnis == soll_ergebnis
+
 def test_schriftliche_note_bestimmen__over_100_raises_error():
     # Arrange
     testwert = 101
