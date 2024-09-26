53971ec9249f2cfc24bfef1368b7b6aa6ad5c7a9
xgvx <grischa.vinogradov@gmail.com>
2024-09-26 09:02:27 +0200

    1 ppositive and 1 negative test

---
+++ Notenberechnung/test_notenberechnung.py
@@ -0,0 +1,28 @@
+from Notenberechnung.notenberechnung import schriftliche_note_bestimmen
+import pytest
+
+def test_schriftliche_note_bestimmen__schmerzen():
+    # Arrange
+    soll_ergebnis = "sehr gut"
+
+    # Act
+    ist_ergebnis = schriftliche_note_bestimmen(100)
+
+    # Assert
+    assert ist_ergebnis == soll_ergebnis
+
+def test_schriftliche_note_bestimmen__over_100_throws():
+    # Arrange
+    testwert = 101
+
+    # Act
+    with pytest.raises(ValueError):
+        schriftliche_note_bestimmen(testwert)
+
+def test_schriftliche_note_bestimmen__negative_number_throws():
+    # Arrange
+    testwert = -1
+
+    # Act
+    with pytest.raises(ValueError):
+        schriftliche_note_bestimmen(testwert)
